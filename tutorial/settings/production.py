d535535d38046b31eaaa474454d07f83e3110813
ldadev <ldadevme7@gmail.com>
2021-09-09 10:25:53 -0500

    agregacion de librerias

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -11,11 +11,20 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {'default': dj_database_url.config()}
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+
+
