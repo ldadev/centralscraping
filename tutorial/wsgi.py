0c373a8e483854d4b78bf5db198b4c95d2df953a
ldadev <ldadevme7@gmail.com>
2021-09-09 01:26:36 -0500

    change wsgi

--- tutorial/wsgi.py
+++ tutorial/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "tutorial.settings" 
+os.environ["DJANGO_SETTINGS_MODULE"] = "tutorial.settings.production" 
 
 application = get_wsgi_application()
 
