b21b1dbf665c6471b657787adb1609cd06929ffd
ldadev <ldadevme7@gmail.com>
2021-09-08 20:47:27 -0500

    conf

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include,url
 
-from django.conf import setting
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
