281b1963aeff37386b26c236ca9f3defd143c433
ldadev <ldadevme7@gmail.com>
2021-09-09 11:52:20 -0500

    agregacion de librerias

--- tutorial/wsgi.py
+++ tutorial/wsgi.py
@@ -1,10 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "tutorial.settings.production" 
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
