aecca0ca3b4dd22180387701f32bb468b2d67cb6
ldadev <ldadevme7@gmail.com>
2021-09-09 13:39:17 -0500

    cambio del setting

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -12,7 +12,11 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {'default': dj_database_url.config()}
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
