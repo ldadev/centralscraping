984916eced08b0a387846ac6308d6043dbcd4e6a
ldadev <ldadevme7@gmail.com>
2021-09-10 11:29:09 -0500

    Modify setting production

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -7,7 +7,7 @@ import dj_database_url
 
 
 #DEBUG =  os.environ.get('DJANGO_DEBUG', default='False')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['tutorialdav.herokuapp.com/','localhost', '127.0.0.1', '0.0.0.0']
 
 
