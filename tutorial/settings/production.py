7f1431ec539cf71188dae036e205c3717f71a914
ldadev <ldadevme7@gmail.com>
2021-09-09 22:43:57 -0500

    modify setting

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {'default': dj_database_url.config()}
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
