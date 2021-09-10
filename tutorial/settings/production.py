be3d711baa34d23418e14902ebb2db4e64608064
ldadev <ldadevme7@gmail.com>
2021-09-09 21:32:20 -0500

    modify setting

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -16,7 +16,7 @@ DATABASES = {'default': dj_database_url.config()}
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
