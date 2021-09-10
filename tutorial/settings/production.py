b55e4f5a4b0cc52e4c434c6951122837cdfbb72f
ldadev <ldadevme7@gmail.com>
2021-09-09 23:40:30 -0500

    modify setting

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -16,13 +16,20 @@ DATABASES = {'default': dj_database_url.config()}
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR ,'static/')]
+#STATIC_URL = 'static/'
+#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR ,'static/')]
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
+STATIC_URL = 'static/'
 
+if not DEBUG: 
+    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
