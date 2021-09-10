2d5aa2687689ef2e72d0c722a436f95e8de433db
ldadev <ldadevme7@gmail.com>
2021-09-09 21:38:01 -0500

    modify setting

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -6,7 +6,7 @@ import django_heroku
 
 
 #DEBUG =  os.environ.get('DJANGO_DEBUG', default='False')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
@@ -16,7 +16,7 @@ DATABASES = {'default': dj_database_url.config()}
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 #STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
