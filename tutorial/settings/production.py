af9f920a826fc7f25853d7186f6e3ff390196327
ldadev <ldadevme7@gmail.com>
2021-09-10 13:53:07 -0500

    Modify setting production

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -19,7 +19,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = ['static/']
 
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 django_heroku.settings(locals())
