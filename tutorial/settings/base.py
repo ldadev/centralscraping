477d2245a2451d2661e25964315c8ae53ecfe5cc
ldadev <ldadevme7@gmail.com>
2021-09-10 12:50:02 -0500

    Modify setting production

--- tutorial/settings/base.py
+++ tutorial/settings/base.py
@@ -4,6 +4,8 @@ import os
 from pathlib import Path
 from decouple import config
 
+WHITENOISE_AUTOREFRESH = True
+
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
