ea77e8b8bd36eecf443908267a53fe91d7ccb062
ldadev <ldadevme7@gmail.com>
2021-09-10 12:33:45 -0500

    Modify setting production

--- tutorial/settings/production.py
+++ tutorial/settings/production.py
@@ -13,6 +13,24 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {'default': dj_database_url.config()}
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
+
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = ['static/']
