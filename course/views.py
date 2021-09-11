6c00f45c70e45194668cc3bab602eb8ba83dbd50
ldadev <ldadevme7@gmail.com>
2021-09-11 13:41:36 -0500

    Modify index and settings productions

--- course/views.py
+++ course/views.py
@@ -8,7 +8,7 @@ def index(request):
 	courses = Course.objects.all()
 	return render(request,'course/main.html',{'courses':courses})
 
-
+"""
 def handler500(request):
     return render(request, 'course/500.html', status=500)
-	
+"""
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include,url
-from django.conf.urls import handler500
 from course.views import *
 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('course.urls')),
     
 ] 
