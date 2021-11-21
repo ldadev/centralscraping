b77a6869c7ecd41ee60e6ca93df36c17efd85a2d
dadev <ldavidme7@gmail.com>
2021-11-21 17:59:28 -0500

    Creación de nueva tabla

--- course/models.py
+++ course/models.py
@@ -3,8 +3,21 @@ from django.db import models
 
 class Course(models.Model):
 
-	title = models.CharField(max_length=50)
-	description = models.TextField()
+	title = models.CharField(max_length=50,verbose_name="Titulo")
+	description = models.TextField(verbose_name="Descripción")
+	
+	def __str__(self):
+		return f'{self.title}'
+
+
+class Teacher(models.Model):
+
+	name = models.CharField(max_length=50,verbose_name="Nombre")
+	course = models.ForeignKey(Course,on_delete=models.CASCADE,verbose_name="Curso")
+	
+	def __str__(self):
+		return f'{self.name}'
+
 
 
 	
